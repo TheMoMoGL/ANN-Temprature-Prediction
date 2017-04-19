1d52e54c085f3b3191a34ba00f3e3b386c219248
ten09003 <ten09003@LEC-CZC528130B.mdh.se>
2017-04-19 08:43:40 +0200

    Sigmoid activation function

---
+++ loadParam.m
@@ -0,0 +1,13 @@
+%%% Function to load parameters from .mat file %%%
+%%% 
+
+
+function [] = loadParam()
+
+load Sunprog2013
+load TempProg2013
+load TempReal2013
+load Time2013
+load WindProg2013
+
+end
