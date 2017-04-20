40f122d952433aecfbb60de872fb2e27f7a84750
tentaz <therese.eriksson1@gmail.com>
2017-04-20 18:02:06 +0200

    Added max and min values as outputs

--- Normalisation.m
+++ Normalisation.m
@@ -1,18 +1,18 @@
 %%% Normalisation of data %%%
 
 
-function [normalT] = Normalisation(T)
+function [normalT, maxVal, minVal] = Normalisation(Parameter)
 
-[rowS, colS] = size(T);
+[rowS, colS] = size(Parameter);
 normalT = zeros(rowS, colS);
 
 % Maximum and minimum value for each column of matrix
-maxVal = max(T);
-minVal = min(T);
+maxVal = max(Parameter);
+minVal = min(Parameter);
 
 % Normalisation of matrix
 c = 1:colS;
 r = 1:rowS;
-normalT(r,c) = (T(r,c) - minVal(c))./(maxVal(c) - minVal(c));
+normalT(r,c) = (Parameter(r,c) - minVal(c))./(maxVal(c) - minVal(c));
 
 end
