b84f8d7ab6c6a1f7907131e3d75f96d620c353cb
FearlessLus <gct14001@student.mdh.se>
2017-04-20 13:29:47 +0200

    Add files via upload
    Error funciton for RMSE , MAPE and corr. coef.

---
+++ Error.m
@@ -0,0 +1,6 @@
+function [] = Error( output, target )
+    RMSE = sqrt( sum( ( target(:) - output(:) ).^2 ) ./ numel(output) );
+    MAPE = abs(sum(( target - output ) ./ target) * (100 ./ numel(output)));
+    Corr = corrcoef( target, output );
+    sprintf('RMSE -> %.4f\nMAPE -> %.4f\nCC -> %.4f\n', RMSE, MAPE, Corr(1,2))
+end
