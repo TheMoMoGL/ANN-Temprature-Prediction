0a960b099889c3455cbf15619a8e5a4fc65eb892
Mujtaba Hasanzadeh <mhh14002@student.mdh.se>
2017-04-21 14:03:27 +0200

    added some comments

--- ANN/Weight_Updator.m
+++ ANN/Weight_Updator.m
@@ -1,8 +1,8 @@
 function [ Updated_weight ] = Weight_Updator( n ,inputs ,Delta_Error ,Weight )
 
-Delta_Weight= inputs* Delta_Error* n;
+Delta_Weight= inputs* Delta_Error* n; % Calculate the Delta weight
 
-Updated_weight = Weight + Delta_Weight;
+Updated_weight = Weight + Delta_Weight; % adding to corresponding weight
 
 
 end
