19085c66bdaf17110122cab9e96329148e0a9ca0
Therése Eriksson <ten09003@student.mdh.se>
2017-04-21 11:42:57 +0200

    Removed pre-processing of real temperatures, changed order on parameters

--- ANNmain.m
+++ ANNmain.m
@@ -1,4 +1,5 @@
-clear all
+close all
+clear
 clc
 % Scaleing parameters
 numInput = 4; % number of input nodes
@@ -7,25 +8,35 @@ n = 0.1; % learning rate
 
 % Concatenate data
 Pwind = importdata('Pwind_training.mat');
-Rtemp = importdata('Rtemp_training.mat');
-Ptemp = importdata('Ptem_training.mat');
 Psun = importdata('Psun_training.mat');
-trainingData = [Pwind, Rtemp, Ptemp, Psun];
+Ptemp = importdata('Ptem_training.mat');
+Rtemp = importdata('Rtemp_training.mat');
+trainingData = [Pwind, Psun, Ptemp, Rtemp];
+
 
 % Outlier detection
-for t = 1:4
+for t = 1:3
     processedTrainingData(:,t) = Pre_process(trainingData(:,t));
 end
 
+processedTrainingData(:,4) = trainingData(:,4);
+
 
 %Training return the weights for validation ANN
-[ inputWeights, hiddenWeights ] = TrainingANN( trainingData, numInput, numHidden, n );
+[ inputWeights, hiddenWeights ] = TrainingANN( processedTrainingData, numInput, numHidden, n );
 
 % Validation with the trained weights
 Pwind = importdata('Pwind_validation.mat');
-Rtemp = importdata('Rtemp_validation.mat');
-Ptemp = importdata('Ptemp_validation.mat');
 Psun = importdata('Psun_validation.mat');
-validationData = [Pwind, Rtemp, Ptemp, Psun];
+Ptemp = importdata('Ptemp_validation.mat');
+Rtemp = importdata('Rtemp_validation.mat');
+validationData = [Pwind, Psun, Ptemp, Rtemp];
 
-[good, bad] = ValidationANN( validationData, inputWeights, hiddenWeights )
+% Outlier detection
+for t = 1:3
+    processedValidationData(:,t) = Pre_process(validationData(:,t));
+end
+
+processedValidationData(:,4) = validationData(:,4);
+
+[good, bad] = ValidationANN( processedValidationData, inputWeights, hiddenWeights )
