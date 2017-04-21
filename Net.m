bbef49ab30f11117ee70677298665dcd8425fd4a
Therése Eriksson <ten09003@student.mdh.se>
2017-04-21 08:34:15 +0200

    Added optional method in comments

--- Net.m
+++ Net.m
@@ -1,7 +1,12 @@
-function [ sum ] = Net( x, y )
-sum = 0;
-for i=1:length(x)
-    sum = sum + (x(i) * y(i));
-end
+function [ summation1 ] = Net( x, y )
+
+summation1 = 0;
+
+for i = 1:length(x)
+    summation1 = summation1 + (x(i) * y(i));
 end
 
+% prod = x.*y;
+% summation2 = sum(prod);
+
+end
