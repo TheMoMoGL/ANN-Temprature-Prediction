64ceb517283a351358d395004528962d99e861e4
Mujtaba Hasanzadeh <mhh14002@student.mdh.se>
2017-04-21 14:00:32 +0200

    Optimised BP

--- ANN/BackP.m
+++ ANN/BackP.m
@@ -9,22 +9,18 @@ Delta_Error_Output=Output_of_the_System*(1-Output_of_the_System)*(Target-Output_
 
 Weigthts_Hidden_inputs=Weigthts_Hidden_inputs';
 %%%%%%%%%%%%%%%Weights updating for Weights between hidden and output
-for i=1:length(Weights_Hidden_Output)
-Delta_Weights_Ouput(i)=Learning_Rate*Delta_Error_Output*Hidden_Nodes(i);
-end 
-Updated_Weights_Hidden_Output=Weights_Hidden_Output+Delta_Weights_Ouput;
+i=1:length(Weights_Hidden_Output);
+[Updated_Weights_Hidden_Output(i)]=Updator_Weights(Learning_Rate,Delta_Error_Output,Hidden_Nodes(i),Weights_Hidden_Output(i));
 
 %%%%%%%%%%%%%%% Calculation of the Delta Error for Hidden nodes
 
-for i=2:length(Hidden_Nodes)
-Delta_Error_Hidden_Nodes(i-1)=Hidden_Nodes(i)*(1 - Hidden_Nodes(i))*Updated_Weights_Hidden_Output(i)*Delta_Error_Output;
-end
+i=1:length(Hidden_Nodes)-1;
+Delta_Error_Hidden_Nodes(i)=Hidden_Nodes(i+1)*(1 - Hidden_Nodes(i+1)')*Updated_Weights_Hidden_Output(i+1)*Delta_Error_Output;
 %%%%Wieghts updating between Hidden and Input layer
 for j=1:length(Hidden_Nodes)-1
  for i=1:length(Input_Nodes)
     
-    Delta_Weights_Input(i,j)=Learning_Rate*Delta_Error_Hidden_Nodes(j)*Input_Nodes(i);
+    [Updated_Weights_Input_Hidden(i,j)]=Updator_Weights(Learning_Rate,Input_Nodes(i),Delta_Error_Hidden_Nodes(j),Weigthts_Hidden_inputs(i,j));
  end 
 end 
-Updated_Weights_Input_Hidden = Weigthts_Hidden_inputs + Delta_Weights_Input;
 Updated_Weights_Input_Hidden=Updated_Weights_Input_Hidden';%Tranponat
