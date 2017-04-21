b82dc810fbf32a25f850d958b314eb511c39ecf6
Gustav Carlstedt <gct14001@student.mdh.se>
2017-04-21 11:05:17 +0200

    Updated validation

--- ValidationANN.m
+++ ValidationANN.m
@@ -1,17 +1,19 @@
 function [good, bad] = ValidationANN( validationData, inputWeights, hiddenWeights ) 
 validationData = Normalisation(validationData);
+row = 1;
 for i=1:4:length(validationData)-96
+    colonn = 1;
     for j=i:4:i+92
-        [input tmpTarget(j)] = HourlyInputTarget( validationData,j+4, i );
-        [ a, b, output(i,j)] = calcOutput( input, inputWeights, hiddenWeights ); 
+        [input tmpTarget(row, colonn)] = HourlyInputTarget( validationData,j+4, i );
+        colonn = colonn + 1;
+        [ a, b, output(row,colonn-1)] = calcOutput( input, inputWeights, hiddenWeights ); 
     end
-    target(i) = tmpTarget(24);
+    row = row + 1;
 end
-%Error( output(:,1), target )
 good = 0;
 bad = 0;
-for i=1:length(target)
-    if abs(output(i, 24) - target(i)) < 0.2209
+for i=1:length(tmpTarget)
+    if abs(output(i,5) - tmpTarget(i,5)) < 0.2517
         good = good+1;
     else
         bad = bad+1;
