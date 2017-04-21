af433d0661c1b4a14903867a44f19e50eb3514a0
Therése Eriksson <ten09003@student.mdh.se>
2017-04-21 12:52:46 +0200

    Added comments, and cleaned up the code a little bit

--- ValidationANN.m
+++ ValidationANN.m
@@ -1,18 +1,31 @@
 function [good, bad] = ValidationANN( validationData, inputWeights, hiddenWeights ) 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Inputs: validationData -> Validation data vector
+%         inputWeights -> weights between input and hidden layer
+%         hiddenWeights -> weights between hidden and output layer
+%
+% Outputs: good -> Accurate temperature forecasts
+%          bad -> Non-accurate temperature forecasts
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 validationData = Normalisation(validationData);
 row = 1;
-for i=1:4:length(validationData)-96
+
+for i = 1:4:length(validationData)-96
     colonn = 1;
-    for j=i:4:i+92
+    for j = i:4:i+92
         [input tmpTarget(row, colonn)] = HourlyInputTarget( validationData,j+4, i );
         colonn = colonn + 1;
-        [ a, b, output(row,colonn-1)] = calcOutput( input, inputWeights, hiddenWeights ); 
+        [~, ~, output(row,colonn-1)] = calcOutput( input, inputWeights, hiddenWeights ); 
     end
     row = row + 1;
 end
+
 good = 0;
 bad = 0;
-for i=1:length(tmpTarget)
+
+for i = 1:length(tmpTarget)
     if abs(output(i,5) - tmpTarget(i,5)) < 0.2517
         good = good+1;
     else
@@ -20,4 +33,5 @@ for i=1:length(tmpTarget)
         
     end
 end
+
 end
