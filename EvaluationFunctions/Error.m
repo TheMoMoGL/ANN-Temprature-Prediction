3c39b6a02ab8ae57ae90f9e8bea51b63f2f76e77
Mohammed Mahmoud <momzor89@gmail.com>
2017-04-24 12:56:35 +0200

    More output paramters

--- EvaluationFunctions/Error.m
+++ EvaluationFunctions/Error.m
@@ -4,6 +4,8 @@ function [RMSE, MAPE, Corr] = Error( output, target )
     RMSE = sqrt( sum( ( target(:) - output(:) ).^2 ) ./ numel(output) );
     MAPE = abs(sum(( target - output ) ./ target) * (100 ./ numel(output)));
     Corr = corrcoef( target, output );
-    sprintf('RMSE -> %.4f\nMAPE -> %.4f\nCC -> %.4f\n', RMSE, MAPE, Corr(1,2))
+    MAPE = MAPE(1,1);
+    Corr = Corr(1,2);
+%     sprintf('RMSE -> %.4f\nMAPE -> %.4f\nCC -> %.4f\n', RMSE, MAPE, Corr(1,2))
     
 end
