b4ae3f51d4165931f098d4e9f88a29d2c2d62ac3
Therése Eriksson <ten09003@student.mdh.se>
2017-04-24 11:08:05 +0200

    Take out every fourth value

--- EvaluationFunctions/graphs.m
+++ EvaluationFunctions/graphs.m
@@ -10,11 +10,11 @@ function [] = graphs(outputVal, actualVal, dateAndTime)
 % Outputs: Noone
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dateAndTimeAlt = dateAndTime(1 : 4 : end);
+dateAndTimeHourly = dateAndTime(1 : 4 : end);
 
 error = outputVal - actualVal;
 
-dt = datetime(dateAndTimeAlt,'inputFormat','uuuu-MM-dd HH:mm','TimeZone','local');
+dt = datetime(dateAndTimeHourly,'inputFormat','uuuu-MM-dd HH:mm','TimeZone','local');
 
 figure('units','normalized','outerposition',[0 0 1 1])
 plot(dt,outputVal)
