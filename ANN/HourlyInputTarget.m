dd541b5996512098b1ad2fbdd1d43ff36d67b9d6
Mohammed Mahmoud <momzor89@gmail.com>
2017-04-24 10:45:55 +0200

    Added some comments

--- ANN/HourlyInputTarget.m
+++
@@ -1,6 +0,0 @@
-function [ input, target ] = HourlyInputTarget( trainingData,indexForecast, indexReal )
-
-     input = [trainingData(indexForecast, 1) trainingData(indexForecast, 2) trainingData(indexForecast, 3)...
-         trainingData(indexReal, 4)];
-     target=trainingData(indexForecast, 4);
-end
