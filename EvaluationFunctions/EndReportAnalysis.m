eee35ca542f2d40f33f4eaf4796c667c3319b54c
Therése Eriksson <ten09003@student.mdh.se>
2017-04-25 09:50:50 +0200

    Takes in more inputs, new axis limit on first plot

--- EvaluationFunctions/EndReportAnalysis.m
+++ EvaluationFunctions/EndReportAnalysis.m
@@ -1,4 +1,4 @@
-function [ output_args ] = EndReportAnalysis( endreport )
+function [] = EndReportAnalysis(endreport, samples, endHidden)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Inputs: endreport -> Report containg all the information [numInput, runHidden, learningRate, good, bad, RMSE, MAPE, Corr]
 %            
@@ -10,6 +10,7 @@ figure
 subplot(2,2,1)       % add first plot in 2 x 2 grid
 stem(endreport(:,2),endreport(:,4))           % stem plot
 title('Good predictions')
+axis([0 endHidden 0 samples])
 
 subplot(2,2,2)       % add second plot in 2 x 2 grid
 stem(endreport(:,2),endreport(:,6))       % stem plot
@@ -21,7 +22,7 @@ title('Mean absolute percentage error')
 
 subplot(2,2,4)       % add fourth plot in 2 x 2 grid
 stem(endreport(:,2),endreport(:,8))           % stem plot
-title('correlation error')
+title('Correlation')
 
 end
 
