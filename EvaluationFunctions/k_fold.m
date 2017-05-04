7938c04c31321cb933acf3b567d6c533514e282b
Therése Eriksson <ten09003@student.mdh.se>
2017-05-04 08:37:38 +0200

    Changed the function

--- EvaluationFunctions/k_fold.m
+++ EvaluationFunctions/k_fold.m
@@ -8,10 +8,19 @@ function [train, test] = k_fold(Observations, K_factor, iterate)
 % Outputs:
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%nrObservations = length(Observations);
+nrObservations = length(Observations);
+partitions = round(nrObservations/K_factor);
 
-%for iterate = 1:K_factor
-    test = Observations(iterate:iterate + K_factor - 1);
-    train = setdiff(Observations, test);
+
+if iterate == 1
+    test = Observations(iterate:partitions);
+    train = Observations(iterate + partitions:end);
+    
+% else if 
+else
+    test = Observations(iterate:iterate + partitions - 1);
+    train = Observations(1:iterate - 1);
+    train = vertcat(train, Observations(iterate + partitions:end));
+
 
 end
