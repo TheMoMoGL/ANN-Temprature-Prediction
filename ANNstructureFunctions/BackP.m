e9a611f197a9beab8c88e510c457a04f83fdd2d1
Mujtaba Hasanzadeh <mhh14002@student.mdh.se>
2017-05-08 08:37:22 +0200

    added some comment

--- ANNstructureFunctions/BackP.m
+++ ANNstructureFunctions/BackP.m
@@ -1,8 +1,7 @@
  function [ Updated_Weights_Input_Hidden,Updated_Weights_Hidden_Output,Hidden_weights ] = BackP(Output_of_the_System,Target,Weights_Hidden_Output,Weigthts_Hidden_inputs,Hidden_Nodes,Input_Nodes,Learning_Rate,Hidden_weights,numberOfhidden)
 
-   [m,n]=size(Hidden_Nodes);                                                                         
-% [ inputWeights,outputWeights, hiddenWeights ] = BackP( output(i), target(i), outputWeights, inputWeights, hiddenOutput, newInput,n,hiddenWeights,numHiddLay);
-%%%%Calculating output error
+[m,n]=size(Hidden_Nodes);                                                                         
+%%%%Calculating output error 
 Delta_Error_Output=(Target-Output_of_the_System);
 %%%%%%%%%%%%%%%Weights updating for Weights between hidden and output
 for i=1:length(Weights_Hidden_Output)
@@ -20,9 +19,10 @@ for i=2:n
 end
 %%%%error calculating for all hidden layer and weights updating
 
-if numberOfhidden > 1
+if numberOfhidden > 1 %% if we have more than 1 hidden layer 
     [r,w]=size(Hidden_weights);
     cnt=r-length(Delta_Error_LastHidden_Nodes)+1;
+    %%% calculating the weights between the last and previous hidden layer
     for j=1:length(Delta_Error_LastHidden_Nodes)
       for u=1:n
         
@@ -32,8 +32,8 @@ if numberOfhidden > 1
       cnt=cnt+1;
     end
     
-    %%%%%%%%%%
-    for d=m-1:-1:1
+    %%%%%% calculating the delta erro of each hidden layer and weight
+    for d=m-1:-1:1 % begin from the next last hiden layer 
    
         for k=2:1:n
             sum_delta=0;
@@ -44,7 +44,7 @@ if numberOfhidden > 1
           
         end
         
-        if d~= 1
+        if d~= 1 %% if not the last hidden layer  then update the weight at this way
             r=r-length(Delta_Error_LastHidden_Nodes);
             cnt=r-length(Delta_Error_LastHidden_Nodes)+1;
             for j=1:length(Delta_Error_LastHidden_Nodes)
@@ -57,7 +57,7 @@ if numberOfhidden > 1
                 end
                 cnt=cnt+1;
             end
-        else
+        else %%% otherwise update the wieghts between input and first hidden layer
             [m,n] = size(Weigthts_Hidden_inputs);
             q=length(Input_Nodes);
             for k=1:length(Delta_Error_LastHidden_Nodes)
@@ -72,7 +72,7 @@ if numberOfhidden > 1
             
         end
     end
-else 
+else %% if we dont have more than one hidden layer then update the weights between the last hidden layer and input 
     
     [m,n] = size(Weigthts_Hidden_inputs);
     q=length(Input_Nodes);
