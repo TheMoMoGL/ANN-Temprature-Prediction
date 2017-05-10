b17ae79c3a6249928bdb212a81329edf919c5677
mhh14002 <mhh14002@LEC-CZC4390GRZ.mdh.se>
2017-05-10 10:59:06 +0200

    solving conflicts

--- GUIANN/ANNGui.m
+++ GUIANN/ANNGui.m
@@ -488,6 +488,9 @@ function percent_CreateFcn(hObject, eventdata, handles)
 
 % Hint: edit controls usually have a white background on Windows.
 %       See ISPC and COMPUTER.
+handles=guidata(hObject);
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+guidata(hObject,handles)
+
