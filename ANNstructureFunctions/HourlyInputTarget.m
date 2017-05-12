5483321116cc7304c94e395bb2450891384ef9ea
gct14001 <gct14001@LEC-CZC528130D.mdh.se>
2017-05-12 08:47:45 +0200

    Cleaned and commented code

--- ANNstructureFunctions/HourlyInputTarget.m
+++ ANNstructureFunctions/HourlyInputTarget.m
@@ -1,6 +1,17 @@
-function [ input, target ] = HourlyInputTarget( trainingData,indexForecast, indexReal,trainingTarget )
+function [ input, target ] = HourlyInputTarget( trainingData, indexForecast, indexReal, trainingTarget )
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% In: trainingData -> Matrix with training data
+%     indexForecast -> Index to take forecast data from in trainingData
+%     indexReal -> Index to take target data from in trainingTarget
+%     trainingTarget -> Vector with all targets (the real temperature)
+% Out: input -> Vector with input parameters
+%      target -> The temperature the 'input' vector is supose
+%                to forecast in the function 'calcOutput'
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 dataSize = size(trainingData);
-     input = [trainingData(indexForecast, 1) trainingData(indexForecast, 2) trainingData(indexForecast, 3)...
+input = [trainingData(indexForecast, 1) trainingData(indexForecast, 2) trainingData(indexForecast, 3)...
          trainingData(indexReal, 4:dataSize(2))];
-     target=trainingTarget(indexForecast);
+target=trainingTarget(indexForecast);
 end
--- Lus-mod/ANNvalidation.m
+++ Lus-mod/ANNvalidation.m
@@ -4,7 +4,6 @@ function [good, bad, RMSE, MAPE, Corr, error, output, target] = ANNvalidation( A
 % Inputs: validationData -> Validation data vector
 %         inputWeights -> weights between input and hidden layer
 %         hiddenWeights -> weights between hidden and output layer
-%
 % Outputs: good -> Accurate temperature forecasts
 %          bad -> Non-accurate temperature forecasts
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -15,7 +14,7 @@ row = 1;
 for ANNcount = 1 : length(A)
     good = 0;
     for i = 1:4:length(A{1,ANNcount}.Data)-time
-        [input, target(row)] = HourlyInputTarget( A{1,ANNcount}.Data, i+time, i,A{1,ANNcount}.Data(:,4) );    
+        [input, target(row)] = HourlyInputTarget( A{1,ANNcount}.Data, i+time, i,A{1,ANNcount}.Data(:,4) );
         output(row) = ReasoningOutput( input, A );
         error(row) = abs(output(row) - target(row));
         if error(row) < 2
@@ -23,29 +22,15 @@ for ANNcount = 1 : length(A)
         end
         row = row + 1;
     end
-  good  
 end
- 
-% Validation of last 24 hours
-% for i = length(validationData)-(92+time) : 4 : length(validationData)
-%     column = 1;
-%     for j = i : 4 : length(validationData)-(time+1)
-%        [input, target(row, column)] = HourlyInputTarget(validationData, j+time, i, trainingTarget);
-%        column = column + 1;
-%        [~, ~, output(row,column-1)] = calcOutput(input, inputWeights, hiddenWeights, outputWeights, numHiddLay);
-%     end
-%     error(row) = abs(output(row) - target(row));
-%     row = row + 1;
-% end
 
 good = 0;
 bad = 0;
 for i = 1:length(target)
-
     if abs(output(i) - target(i)) < 2
         good = good+1;
     else
-        bad = bad+1;     
+        bad = bad+1;
     end
 end
 [RMSE, MAPE, Corr] = Error(output, target);
