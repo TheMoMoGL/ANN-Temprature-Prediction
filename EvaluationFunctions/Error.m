c3548864ec5c766e1330d5ec95f7fd50559ea637
gct14001 <gct14001@LEC-02313.mdh.se>
2017-05-18 14:33:11 +0200

    Error, third times the charm woopwoop

--- EvaluationFunctions/Error.m
+++ EvaluationFunctions/Error.m
@@ -11,7 +11,8 @@ function [RMSE, MAPE, Corr] = Error( output, target )
 n = numel(target);
 
 RMSE = sqrt(mean((target(:) - output(:)).^2)); % Actual root mean square error function
-MAPE = abs(sum((output - target)./ output)) * (100/n); % times 100 to get result in percent
+%MAPE = sum(abs((output - target)./ output)) * (1/n); % times 100 to get result in percent
+MAPE = ((100/n) * sum(abs(target-output)./abs(target)));
 Corr = corrcoef(target, output);
 Corr = Corr(1,2); % 2x2 matrix with ones in the diagonal corr. coef. value in (1,2) & (2,1)
 
