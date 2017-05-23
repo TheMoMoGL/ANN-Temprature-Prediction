c61f5cbabaf45664c784786c4735775c6bc4182a
mhh14002 <mhh14002@LEC-02308.mdh.se>
2017-05-23 09:09:26 +0200

    changed to tnh

--- EvaluationFunctions/Error.m
+++ EvaluationFunctions/Error.m
@@ -9,11 +9,9 @@ function [RMSE, MAPE, Corr] = Error( output, target )
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 n = numel(target);
-
 RMSE = sqrt(mean((target(:) - output(:)).^2)); % Actual root mean square error function
-
 %MAPE = sum(abs((output - target)./ output)) * (1/n); % times 100 to get result in percent
-MAPE = ((100/n) * sum(abs(target-output)./abs(target)));
+MAPE = (100/n) * (sum(abs(target-output))/sum(target));
 Corr = corrcoef(target, output);
 Corr = Corr(1,2); % 2x2 matrix with ones in the diagonal corr. coef. value in (1,2) & (2,1)
 
